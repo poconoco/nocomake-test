8fe9554d2f8b52e970c8b0024bdabb3aa80fd454
Leonid Yurchenko <noco.mi@gmail.com>
2024-04-12 09:52:50 +0300

    Fix ranges, cleanup

--- tft-joystick/tft-joystick.ino
+++ tft-joystick/tft-joystick.ino
@@ -1,12 +1,19 @@
+/*
+ * (c) Leonid Yurchenko
+ * https://www.youtube.com/@nocomake
+ */
+
 #include <TFT.h>
 #include <SPI.h>
 
-#define CS   10
-#define DC   9
+#define CS     10
+#define DC     9
 #define RESET  8
 
-#define JOY_X 0
-#define JOY_Y 1
+#define JOY_X  0
+#define JOY_Y  1
+
+#define R 10
 
 TFT myScreen = TFT(CS, DC, RESET);
 int prevX = -100;
@@ -16,26 +23,24 @@ void setup() {
   myScreen.begin();
   myScreen.setRotation(0);
   myScreen.background(0,0,0);
-  delay(200);
 }
-
 
 void loop() {
   int x = analogRead(JOY_X);
   int y = analogRead(JOY_Y);
 
-  x = map(x, 0, 1024, 10, myScreen.width()-10);
-  y = map(y, 0, 1024, myScreen.height()-11, 9);
+  x = map(x, 0, 1023, R, myScreen.width() - R - 1);
+  y = map(y, 0, 1023, myScreen.height() - R - 1, R);
 
-  myScreen.stroke(0, 0, 0);
-  myScreen.circle(prevX, prevY, 10);
+  if (prevX != x || prevY != y) {  // Lowers the flickering
+    myScreen.stroke(0, 0, 0);
+    myScreen.circle(prevX, prevY, R);
 
-  myScreen.stroke(255,255,255);
-  myScreen.circle(x, y, 10);
-
-  prevX = x;
-  prevY = y;
+    myScreen.stroke(255,255,255);
+    myScreen.circle(x, y, R);
+    prevX = x;
+    prevY = y;
+  }
 
   delay(20);
 }
-

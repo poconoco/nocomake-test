b828b36b7b75da84136cf629c9510886c7cb0001
Leonid Yurchenko <noco.mi@gmail.com>
2025-05-13 23:06:56 +0300

    Fill IK example

--- py-3dof-test/run.py
+++ py-3dof-test/run.py
@@ -1,21 +1,79 @@
+import yaml
+import board
+import time
+
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
+
 from submodules.py_kinematics.ik3dof import IK3DOF
+from submodules.py_kinematics.point3d import Point3D
+
+
+class Leg:
+    def __init__(self, leg_config: dict, pca: PCA9685):
+        self.ik = IK3DOF()
+        self.ik.coxa_h_offset = leg_config['coxa'].get('h_offset', 0.0)
+        self.ik.coxa_v_offset = leg_config['coxa'].get('v_offset', 0.0)
+
+        self.ik.femur_length = leg_config['femur'].get('length', 0.0)
+        self.ik.tibia_length = leg_config['tibia'].get('length', 0.0)
+
+        self.ik.coxa_angle_for_sideway = leg_config['coxa']['angle_for_sideway']
+        self.ik.femur_angle_for_horizontal = leg_config['femur']['angle_for_horizontal']
+
+        # angle_phisical_delta is used for femur that are Г-shaped, and it's easier to 
+        # calibrate angles for the straight part of the tibia, but the real tibia is diagonal
+        # between the ends of Г
+        self.ik.tibia_angle_for_femur_parallel = leg_config['tibia']['angle_for_femur_parallel'] + \
+            leg_config['tibia'].get('angle_phisical_delta', 0.0) * leg_config['tibia'].get('multiplier', 1.0) 
+
+        self.ik.coxa_multiplier = leg_config['coxa'].get('multiplier', 1.0)
+        self.ik.femur_multiplier = leg_config['femur'].get('multiplier', 1.0)
+        self.ik.tibia_multiplier = leg_config['tibia'].get('multiplier', 1.0)
+
+        self.coxa_servo = self._get_servo(leg_config['coxa']['servo'], pca)
+        self.femur_servo = self._get_servo(leg_config['femur']['servo'], pca)
+        self.tibia_servo = self._get_servo(leg_config['tibia']['servo'], pca)
+
+    def reach(self, point: Point3D):
+        coxa_angle, femur_angle, tibia_angle = self.ik.get_angles(point)
+        if coxa_angle is not None:
+            self.coxa_servo.angle = coxa_angle
+
+        if femur_angle is not None:    
+            self.femur_servo.angle = femur_angle
+
+        if tibia_angle is not None:
+            self.tibia_servo.angle = tibia_angle
+
+    def detach(self):
+        self.coxa_servo.angle = None
+        self.femur_servo.angle = None
+        self.tibia_servo.angle = None
+        
+
+    def _get_servo(self, servo_config, pca):
+        return servo.Servo(pca.channels[servo_config['channel']], min_pulse=servo_config['min_pulse'], max_pulse=servo_config['max_pulse'])
 
 
 def main():
-    ik = IK3DOF()
+    with open('config.yaml', 'r') as config_file:
+        config = yaml.safe_load(config_file)
 
-    ik.coxa_h_offset = None
-    ik.coxa_v_offset = None
-    
-    ik.femur_length = None
-    ik.tibia_length = None
+    i2c = board.I2C()
+    pca = PCA9685(i2c)
+    pca.frequency = 50
 
-    ik.coxa_sideway_angle = None
-    ik.femur_horizontal_angle = None
-    ik.tibia_femur_parallel_angle = None
+    left_leg = Leg(config['left_first'], pca)
+    right_leg = Leg(config['right_first'], pca)
 
+    left_leg.reach(Point3D(10, 10, -50))
 
-    print("Hello, world!")
+    time.sleep(1)
+    left_leg.detach()
+
+    pca.deinit()
 
 
 if __name__ == "__main__":

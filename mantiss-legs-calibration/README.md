b68b11ed2061c9d592ef7634c494c9aa8d4f9e91
Leonid Yurchenko <noco.mi@gmail.com>
2026-01-03 12:47:49 +0200

    Add mantiss servo calibration code

---
+++ mantiss-legs-calibration/README.md
@@ -0,0 +1,33 @@
+# Requirements
+
+This is to be runned on raspberry pi or similar system that has access to i2c
+
+# Preparation
+
+## Create venv
+
+```
+python3 -m venv .venv
+```
+
+## Activate venv
+
+```
+source .venv/bin/activate
+```
+
+## Install dependencies
+
+```
+python -m pip install -r requirements.txt
+```
+
+# Test angles script
+
+This script is to find your angles and directions to konfigure IK3DOF class 
+angular parameters.
+
+```
+python test_angles.py
+```
+
---
+++ mantiss-legs-calibration/test_angles.py
@@ -0,0 +1,50 @@
+import yaml
+import time
+import board
+
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
+
+def get_servo(pca, servo_config):
+    return servo.Servo(pca.channels[servo_config['channel']], min_pulse=servo_config['min_pulse'], max_pulse=servo_config['max_pulse'])
+
+
+
+def move(servo_, servo_config, angle):
+    servo_angle = servo_config['rest_angle'] + (servo_config.get('angle_phisical_delta', 0) + angle) * servo_config.get('multiplier', 1)
+    print(f'Servo moved to angle: {servo_angle}')
+    servo_.angle = servo_angle
+
+    time.sleep(2)
+
+    servo_.angle = None
+    print("Servo deinitialized.")
+
+def main():
+
+    with open('config.yaml', 'r') as config_file:
+        config = yaml.safe_load(config_file)
+
+#    servo_config = config['tail']
+
+    servo_config = config['legs']['left_middle']['coxa']['servo']
+
+
+    i2c = board.I2C()
+    pca = PCA9685(i2c_bus=i2c, address=servo_config.get('i2c_address'))
+    pca.frequency = 50
+
+    test_servo = get_servo(pca, servo_config)
+
+#    move(pca, config, 'left_first', -90, -60, -90)
+    move(test_servo, servo_config, -45)
+    move(test_servo, servo_config, 45)
+    move(test_servo, servo_config, -45)
+    move(test_servo, servo_config, 45)
+
+    pca.deinit()
+
+
+if __name__ == "__main__":
+    main()
